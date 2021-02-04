25afb9932e333a8f567c857bd5583ffa26a91520
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-02-04 18:30:50 +0100

    exercice 2 partie 1

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/cours', function () {
-    return \App\Models\Cour::all();
+//on récupère tous les éléments
+
+Route::get('/cours', function(Request $request) {
+    $fields = $request->get('fields') ? explode('_',$request->get('fields')) :'*';
+    return \App\Models\Cour::select($fields)->get();
 });
 
 
@@ -27,15 +30,20 @@ Route::get('/cours/{courId}', function ($courId) {
     return \App\Models\Cour::find($courId);
 });
 
+//on remplace un ou plusieurs éléments par une nouvelle version
+
 Route::put('/cours/{courId}', function ($courId, Request $request) {
     $cour = \App\Models\Cour::find($courId);
     return $cour->update($request->all());
 });
 
+//on crée un élément dans la ressource
+
 Route::post('/cours', function (Request $request) {
     return \App\Models\Cour::created($request->all());
 });
 
+//on supprime un ou plusieurs éléments
 
 Route::delete('/cours/{courId}', function ($courId) {
     return \App\Models\Cour::find($courId)->delete();
